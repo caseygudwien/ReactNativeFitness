baa2604c43418e720a6516dc839cc555679189f7
caseygudwien <caseygudwien@gmail.com>
2025-08-15 20:30:39 -0500

    removed rate limiter

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-app.use(rateLimiter);
+// app.use(rateLimiter);
 app.use(express.json());
 
 app.use("/api/workouts", workoutRoute);
